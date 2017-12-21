36f3c812cd6e2691436a7867bf5ac8b893188ac5
Wojtek Pyda <w.pyda@poczta.onet.pl>
2017-12-21 15:00:20 +0100

    map, filter

---
+++ script.js
@@ -0,0 +1,141 @@
+// **simple function**
+// function snow() {
+//
+// }
+
+// // **simple function returns a**
+// function snow(a) {
+//     return a
+// }
+//
+// var b = 'sniezynka';
+// function snow(a) {
+//     return b
+// }
+// // returns sniezynka
+
+// function declaration
+// function add(a, b) {
+//     return a + b
+// }
+
+// // funkcja rekurencyjna, wywolujemy funkcje w funkcji
+// function count(x) {
+//     console.log(x);
+//     if (x > 0) {
+//         count(x-1);
+//     }
+// }
+//
+// // wyswietla liczby od do
+// function countDownFrom(from, to) {
+//     if (from < to)
+//         throw Error('Djdajdnjand');         // przy else if nizej, console log nadal dziala i wyswietla 'from'
+//     console.log(from);
+//     if (from > to) {
+//         countDownFrom(from-1, to);
+//     }
+// }
+
+// // funkcje w funckji
+// var addSquares = function (x, y) {
+//     var square = function (z) {
+//         return z*z
+//     }
+//     return square(x) + square(y)
+// }
+
+// var makeAlwaysAddFunction = function (numberThatWillBeAdded) {
+//     var alwaysAddFunction = function (x) {
+//         return x + numberThatWillBeAdded
+//     }
+//     return alwaysAddFunction
+// }
+
+// var higherOrderFunction = function (otherFunction) {
+//     var sum = 1 + 3 + 4;
+//     otherFunction(sum)
+// };
+//
+// var loggingFunction = function (message) {
+//     console.log(message)
+// };
+//
+// higherOrderFunction(loggingFunction());         // logs 8
+
+// c) logging out all array numbers using for
+// var array1 = [1,2,3,4,5];
+//
+// for(var i = 0; i < array1.length; i++) {
+//     console.log(array1[i])
+// }
+
+// c) logging out all array numbers using forEach
+// var array2 = [10,20,30,40,50];
+//
+// array2.forEach(function (el, i, arr) {
+//     console.log(el)
+// });
+
+// var ourOwnForEach = function (array, fnForEveryElement) {
+//     for (var i = 0; i <  array.length; i++) {
+//         // var element = array[i];          // element
+//         // var index = i;                   // index
+//         // array                            // array
+//         fnForEveryElement(array[i], i, array)
+//     }
+// };
+
+/// MAP
+
+// var numbers = [10, 20, 30, 40, 50];
+//
+// numbers.map(function (element, index, array) {
+//     return element * 2
+// });                                                 // zwraca NOWA tablice [20, 40, 60, 80, 100] - elemety tablicy numberts x2
+
+/// FILTER
+
+// var numbers = [11, 22, 33, 44, 55];
+//
+// // filter makes NEW array that contains ONLY that elements
+// // for which passed function return true
+//
+// // only even numbers (parzyste)
+//
+// var newNumbers = numbers.filter(function (el) {
+//     return el % 2 === 0;
+// });
+//
+// // only odd numbers (nieparzyste)
+//
+// var oddNumbers = numbers.filter(function (el) {
+//     return el % 2 !== 0;
+// });
+
+// var people = [
+//     {name: "Mateusz", gender: "male", team: null},
+//     {name: "Waldek", gender: "male", team: "FitCode"},
+//     {name: "Tomek", gender:"male", team: "FitCode"},
+//     {name: "Natalia", gender: "female", team: "FitCode"},
+//     {name: "Wojtek", gender: "male", team: "FitCode"},
+//     {name: "Grzesiek", gender: "male", team: "WeeBees"},
+//     {name: "Paweł", gender: "male", team: "WeeBees"},
+//     {name: "Kasia", gender: "female", team: "WeeBees"},
+//     {name: "Ola", gender: "female", team: "WeeBees"}
+// ];
+//
+// var onlyFemales = people.filter(function (person) {             // person = element
+//     return person.gender === "female"
+// });
+//
+// var onlyMales = people.filter(function (person) {             // person = element
+//     return person.gender === "male"
+// });
+//
+// var onlyMalesWeeBees = people.filter(function (person) {             // person = element
+//     return person.gender === "male" && person.team === "WeeBees"
+// });
+
+/// REDUCE
+
